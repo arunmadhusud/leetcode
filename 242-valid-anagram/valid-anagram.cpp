9a90d10477681f938fa41eae7fa087106a6e7d96
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-09-10 10:44:08 -0400

    Time: 10 ms (46.06%) | Memory: 8.8 MB (55.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,8 +3,12 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        if (s == t) return true;
-        else return false;
+        if (t == s)
+        {
+            return true;
+        }
+        return false;
+
         
     }
 };
