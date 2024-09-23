47fe2ae876457886a3d89f35f6783252de964955
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-09-23 17:19:58 -0400

    Time: 11 ms (45.42%) | Memory: 10.7 MB (86.34%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -6,20 +6,37 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        if (head==nullptr) return false;
-        unordered_set<ListNode*> visited;
-        visited.insert(head);
-        ListNode* curr = head->next;
-        while (curr!=nullptr){
-            if(visited.find(curr) != visited.end()) return true;
-            else visited.insert(curr);
-            curr = curr->next;
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if (head==nullptr) return false;
+//         unordered_set<ListNode*> visited;
+//         visited.insert(head);
+//         ListNode* curr = head->next;
+//         while (curr!=nullptr){
+//             if(visited.find(curr) != visited.end()) return true;
+//             else visited.insert(curr);
+//             curr = curr->next;
 
-        }
-        return false;
+//         }
+//         return false;
         
-    }
+//     }
+// };
+
+class Solution {
+    public:
+        bool hasCycle(ListNode* head){
+            if (head == nullptr) return false;
+            ListNode* slowPtr = head;
+            ListNode* fastPtr = head;
+
+            while(fastPtr!=nullptr && fastPtr->next!=nullptr)
+            {
+                slowPtr = slowPtr->next;
+                fastPtr = fastPtr->next->next;
+                if(slowPtr==fastPtr) return true;
+            }
+            return false;
+        }
 };
