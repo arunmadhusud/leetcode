36cfde15778baae55ca00a1eea0cfa786249a3d8
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-09-23 16:16:41 -0400

    Time: 21 ms (35.08%) | Memory: 14.7 MB (28.34%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,18 +5,11 @@ public:
         int start = 0;
         int maxLength = 0;
         for (char elem : s){
-            if(subString.find(elem) == subString.end())
-            {
-                subString.insert(elem);
-            }
-            else
-            {
-                while(subString.find(elem) != subString.end()){
+            while(subString.find(elem) != subString.end()){
                     subString.erase(s[start]);
                     start++;
                 }
-                subString.insert(elem);
-            }
+            subString.insert(elem);     
             maxLength = max(maxLength,(int)subString.size());
         }
         return maxLength;
