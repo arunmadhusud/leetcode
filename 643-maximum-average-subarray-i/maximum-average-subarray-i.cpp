031c76c5f5c2cb64afe7ae05187118bc937a0952
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-09-23 14:21:59 -0400

    Time: 126 ms (53.85%) | Memory: 112.8 MB (8.32%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -6,12 +6,10 @@ public:
         for (int i=0; i < k; i++){
             sum +=nums[i];
         }
-        std::cout << sum << endl;
         double max_sum = sum;
         for (int i=k; i < nums.size();i++){
             sum += nums[i]-nums[start];
             max_sum = max(max_sum,sum);
-            std::cout << max_sum << endl;
             start++;
         }
         return max_sum/k;
