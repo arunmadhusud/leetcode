1e8fc5a956c659264aedb4b2c1d9f4e62ad5139a
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-09-23 16:50:12 -0400

    Time: 15 ms (14.02%) | Memory: 14.5 MB (13.74%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,20 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (!head || !head->next) return false; // Check if the list is empty or has only one element
+        if (head==nullptr) return false;
+        unordered_set<ListNode*> visited;
+        visited.insert(head);
+        ListNode* curr = head->next;
+        while (curr!=nullptr){
+            if(visited.find(curr) != visited.end()) return true;
+            else visited.insert(curr);
+            curr = curr->next;
 
-        ListNode* slow = head;  // Tortoise (moves one step at a time)
-        ListNode* fast = head;  // Hare (moves two steps at a time)
-
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;           // Move slow pointer by one step
-            fast = fast->next->next;     // Move fast pointer by two steps
-
-            if (slow == fast) {          // If they meet, there is a cycle
-                return true;
-            }
         }
-
-        return false;  // If the fast pointer reaches the end, no cycle exists
+        return false;
+        
     }
 };
