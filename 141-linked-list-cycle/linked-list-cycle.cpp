10eb935ca224b9584fa4eee6bc96dec507d87106
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-10-06 09:41:04 -0400

    Time: 8 ms (72.08%) | Memory: 10.9 MB (66.57%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -6,37 +6,17 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-// class Solution {
-// public:
-//     bool hasCycle(ListNode *head) {
-//         if (head==nullptr) return false;
-//         unordered_set<ListNode*> visited;
-//         visited.insert(head);
-//         ListNode* curr = head->next;
-//         while (curr!=nullptr){
-//             if(visited.find(curr) != visited.end()) return true;
-//             else visited.insert(curr);
-//             curr = curr->next;
-
-//         }
-//         return false;
-        
-//     }
-// };
-
 class Solution {
-    public:
-        bool hasCycle(ListNode* head){
-            if (head == nullptr) return false;
-            ListNode* slowPtr = head;
-            ListNode* fastPtr = head;
-
-            while(fastPtr!=nullptr && fastPtr->next!=nullptr)
-            {
-                slowPtr = slowPtr->next;
-                fastPtr = fastPtr->next->next;
-                if(slowPtr==fastPtr) return true;
-            }
-            return false;
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slwptr = head;
+        ListNode* fstptr = head;
+        while(fstptr!=nullptr && fstptr->next!=nullptr){
+            slwptr = slwptr->next;
+            fstptr = fstptr->next->next;
+            if(fstptr==nullptr) return false;
+            if(fstptr==slwptr) return true;
         }
+        return false;        
+    }
 };
