6fc7664eb888ce767b9e4365ccdea370ac986ce2
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-10-07 10:37:48 -0400

    Time: 2 ms (49.89%) | Memory: 10.6 MB (45.42%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -11,38 +11,26 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if (!head || left == right) return head;
-
         ListNode* prev = nullptr;
         ListNode* curr = head;
-
-        // Move to the node at position `left`
-        for(int i = 0; i < left - 1; i++) {
+        for(int i = 0; i < left - 1; i++){
             prev = curr;
             curr = curr->next;
         }
-
-        // Mark the nodes at the start and before the start of the reverse sublist
-        ListNode* beforeLeft = prev;
+        ListNode* prevNode = prev;
         ListNode* leftNode = curr;
 
-        // Reverse the sublist between `left` and `right`
         ListNode* tmp = nullptr;
-        for (int i = 0; i <= right - left; i++) {
+        for(int k = 0; k <= right- left; k++){
             tmp = curr->next;
             curr->next = prev;
             prev = curr;
             curr = tmp;
         }
-
-        // Reconnect the reversed sublist with the rest of the list
-        if (beforeLeft != nullptr) {
-            beforeLeft->next = prev;
-        } else {
-            head = prev;  // Update head if reversing starts at position 1
-        }
+        if(prevNode!=nullptr) prevNode->next = prev;
+        else head = prev;
         leftNode->next = curr;
-
         return head;
     }
+
 };
