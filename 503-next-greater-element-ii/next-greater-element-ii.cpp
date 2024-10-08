0b8be2ba952597af0cd670d999017545f90ed7d0
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-10-08 18:40:48 -0400

    Time: 17 ms (87.40%) | Memory: 26.7 MB (75.79%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> result(nums.size(),-1);
+        stack<int> stIdx;
         int n = nums.size();
-        vector<int> result(n, -1);  // Initialize result array with -1
-        stack<int> elem_stack;  // Monotonic decreasing stack to store indices
-        
-        // Traverse the array twice to simulate the circular behavior
-        for (int i = 0; i < 2 * n; i++) {
-            int num = nums[i % n];  // Circular array, use modulo to wrap around
-            
-            // If the current element is greater than the element at the index stored at the top of the stack
-            while (!elem_stack.empty() && nums[elem_stack.top()] < num) {
-                result[elem_stack.top()] = num;  // Update the result for the index at the top
-                elem_stack.pop();
+
+        for(int i = 0; i <  2 * n; i++){
+            int num = nums[i % n];
+            while(!stIdx.empty() && nums[stIdx.top()]<num){
+                result[stIdx.top()] = num;
+                stIdx.pop();
             }
-            
-            // Only push indices in the first pass (first n elements)
-            if (i < n) {
-                elem_stack.push(i);
-            }
+            if(i<n) stIdx.push(i);
         }
+
+        return result;
         
-        return result;  // Return the result array
     }
 };
