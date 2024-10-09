e97b94e036cfbfd9b4a01b07a878ec1c02f51702
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-10-09 11:45:06 -0400

    Time: 0 ms (100.00%) | Memory: 7.3 MB (95.74%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-       return fib(n+1);
-    }
-    int fib(int n) {
         if (n < 2) return n;
         
         vector<int> table(2, 0); // Size of 2 to hold fib(n-2) and fib(n-1)
+        table[0] = 1;
         table[1] = 1; // base case: fib(1) = 1
         
         // Use a variable to temporarily hold the previous value
         int prev;
         
-        for (int i = 2; i <= n; i++) {
+        for (int i = 0; i < n-1; i++) {
             prev = table[1];                  // Store the current fib(n-1)
             table[1] = table[0] + table[1];  // Calculate fib(n)
             table[0] = prev;                  // Update fib(n-2)
