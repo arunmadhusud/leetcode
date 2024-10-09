c656d32b49f2563655a41b2ed5b55eddf80df741
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-10-09 11:47:17 -0400

    Time: 0 ms (100.00%) | Memory: 7.6 MB (49.04%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n < 2) return n;
-        
-        vector<int> table(2, 0); // Size of 2 to hold fib(n-2) and fib(n-1)
-        table[0] = 1;
-        table[1] = 1; // base case: fib(1) = 1
+        if (n < 2) return 1;  // There is one way to climb 0 or 1 step
+
+        vector<int> table(2, 0); // Size of 2 to hold steps for last and second last step
+        table[0] = 1; // Ways to climb 0 steps
+        table[1] = 1; // Ways to climb 1 step
         
         // Use a variable to temporarily hold the previous value
         int prev;
         
-        for (int i = 0; i < n-1; i++) {
-            prev = table[1];                  // Store the current fib(n-1)
-            table[1] = table[0] + table[1];  // Calculate fib(n)
-            table[0] = prev;                  // Update fib(n-2)
+        // Start the loop from 2 to n (inclusive)
+        for (int i = 2; i <= n; i++) {
+            prev = table[1];                 // Store the second last step
+            table[1] = table[0] + table[1];  // Calculate current steps
+            table[0] = prev;                  // Update second last step
         }
         
-        return table[1]; // Return the nth Fibonacci number
+        return table[1];  // Return the total ways to climb n steps
     }
 };
