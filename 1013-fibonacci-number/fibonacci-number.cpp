c7a9d1953cf7c6509c02d1bac06c49ca6a6c7daa
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-10-09 10:45:49 -0400

    Time: 3 ms (66.17%) | Memory: 7.5 MB (12.78%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -18,15 +18,20 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<2) return n;
-        vector<int> table(2,0);
-        table[1] = 1;
+        if (n < 2) return n;
+        
+        vector<int> table(2, 0); // Size of 2 to hold fib(n-2) and fib(n-1)
+        table[1] = 1; // base case: fib(1) = 1
+        
+        // Use a variable to temporarily hold the previous value
         int prev;
-        for(int i = 2; i <=n; i++){
-            prev = table[1];
-            table[1] = table[0]+table[1];
-            table[0] = prev;
+        
+        for (int i = 2; i <= n; i++) {
+            prev = table[1];                  // Store the current fib(n-1)
+            table[1] = table[0] + table[1];  // Calculate fib(n)
+            table[0] = prev;                  // Update fib(n-2)
         }
-        return table[1];        
+        
+        return table[1]; // Return the nth Fibonacci number
     }
 };
