1a9e43ecf126eb27ef5b2ebf873235ae48143e35
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-10-19 10:37:49 -0400

    Time: 47 ms (9.34%) | Memory: 26.2 MB (8.27%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,13 +4,13 @@ public:
         int n = text1.size();
         int m = text2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        for(int i = 0; i<=n; i++) dp[i][0] = 0;
-        for(int j = 0; j<=m; j++) dp[0][j] = 0;
+        // for(int i = 0; i<=n; i++) dp[i][0] = 0;
+        // for(int j = 0; j<=m; j++) dp[0][j] = 0;
         return lcs(text1,text2,n,m,dp);
         
     }
     int lcs(string& text1, string& text2,int n,int m,vector<vector<int>>& dp){
-        // if(n==0 || m== 0) return 0;
+        if(n==0 || m== 0) return 0;
         if(dp[n][m] !=-1) return dp[n][m];
         if(text1[n-1]==text2[m-1]) dp[n][m] = 1+ lcs(text1,text2,n-1,m-1,dp);
         else {
