c902bff13997e22a0bc8865a31965903e5a8a4f2
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-12-17 11:55:46 -0500

    Time: 29 ms (50.77%) | Memory: 18.5 MB (40.51%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<int>> visited(grid.size(), vector<int>(grid[0].size(), 0));
-        queue<std::pair<int,int>> q;
-        
+        std::vector<std::vector<bool>> visited(grid.size(),std::vector<bool>(grid[0].size(),false));
+        std::queue<std::pair<int, int>> q;
         int islandCount = 0;
-        for (int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if (grid[i][j] == '1' && visited[i][j] == 0){
+        for (int i = 0; i<grid.size(); i++){
+            for (int j =0; j<grid[0].size(); j++){  
+                if(!visited[i][j] && grid[i][j] == '1' ) {             
                     q.push({i,j});
+                    visited[i][j] = true;                    
                     while(!q.empty()){
-                        std::pair<int,int> curr = q.front();
+                        std::pair<int, int> curr = q.front();
                         q.pop();
                         int r = curr.first;
                         int c = curr.second;
-                        // Check all four neighbors
-                        if (r - 1 >= 0 && visited[r - 1][c] == 0 && grid[r - 1][c] == '1') { // Up
-                            q.push({r - 1, c});
-                            visited[r - 1][c] = 1;
-                            // isIsland = true;
+                        if ((r-1)>=0 && !visited[r-1][c] && grid[r-1][c] == '1')
+                        {
+                            q.push({r-1,c});
+                            visited[r-1][c] = true;
                         }
-                        if (r + 1 < grid.size() && visited[r + 1][c] == 0 && grid[r + 1][c] == '1') { // Down
-                            q.push({r + 1, c});
-                            visited[r + 1][c] = 1;
-                            // isIsland = true;
+                        if ((r+1)<grid.size() && !visited[r+1][c] && grid[r+1][c] == '1')
+                        {
+                            q.push({r+1,c});
+                            visited[r+1][c] = true;
+                        }   
+                        if ((c-1)>=0 && !visited[r][c-1] && grid[r][c-1] == '1')
+                        {
+                            q.push({r,c-1});
+                            visited[r][c-1] = true;
+                        } 
+                        if ((c+1)<grid[0].size() && !visited[r][c+1] && grid[r][c+1] == '1')
+                        {
+                            q.push({r,c+1});
+                            visited[r][c+1] = true;
                         }
-                        if (c - 1 >= 0 && visited[r][c - 1] == 0 && grid[r][c - 1] == '1') { // Left
-                            q.push({r, c - 1});
-                            visited[r][c - 1] = 1;
-                            // isIsland = true;
-                        }
-                        if (c + 1 < grid[0].size() && visited[r][c + 1] == 0 && grid[r][c + 1] == '1') { // Right
-                            q.push({r, c + 1});
-                            visited[r][c + 1] = 1;
-                            // isIsland = true;
-                        }                                           
+
                     }
-                    islandCount++;
-                }
+                    islandCount++;                                      
+
+                }                    
+                
             }
         }
-        return islandCount;
-        
+    return islandCount;    
     }
 };
