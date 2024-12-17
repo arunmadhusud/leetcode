407a06e4b54b31575c3731bee5d089f52c86cd3a
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-12-17 12:24:17 -0500

    Time: 0 ms (100.00%) | Memory: 18.6 MB (6.76%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<bool>> visited(image.size(),vector<bool>(image[0].size(),false));
-        queue<std::pair<int,int>> q;
-        int actColor = image[sr][sc];
-        q.push({sr,sc});
-        visited[sr][sc] = true;
-        image[sr][sc] = color;
+        std::vector<std::vector<bool>> visited(image.size(), std::vector<bool>(image[0].size(), false));
+        std::queue<std::pair<int,int>> q;
 
-        while(!q.empty()){
+        int currColor = image[sr][sc];
+        q.push({sr,sc});
+        image[sr][sc] = color;
+        visited[sr][sc] = true;
+
+        while(!q.empty())
+        {
             std::pair<int,int> curr = q.front();
             q.pop();
             int r = curr.first;
             int c = curr.second;
-            // Check all four neighbors
-            if (r - 1 >= 0 && !visited[r - 1][c] && image[r - 1][c] == actColor) { // Up
-                q.push({r - 1, c});
-                visited[r - 1][c] = true;
-                image[r - 1][c] = color;
+
+            if ((r-1)>=0 && !visited[r-1][c] && image[r-1][c] == currColor)
+            {
+                q.push({r-1,c});
+                visited[r-1][c] = true;
+                image[r-1][c] = color;
             }
-            if (r + 1 < image.size() && !visited[r + 1][c] && image[r + 1][c] == actColor) { // Down
-                q.push({r + 1, c});
-                visited[r + 1][c] = true;
-                image[r + 1][c] = color;
+            if ((r+1)<image.size() && !visited[r+1][c] && image[r+1][c] == currColor)
+            {
+                q.push({r+1,c});
+                visited[r+1][c] = true;
+                image[r+1][c] = color;
+            }   
+            if ((c-1)>=0 && !visited[r][c-1] && image[r][c-1] == currColor)
+            {
+                q.push({r,c-1});
+                visited[r][c-1] = true;
+                image[r][c-1] = color;
+            } 
+            if ((c+1)<image[0].size() && !visited[r][c+1] && image[r][c+1] == currColor)
+            {
+                q.push({r,c+1});
+                visited[r][c+1] = true;
+                image[r][c+1] = color;
             }
-            if (c - 1 >= 0 && !visited[r][c - 1] && image[r][c - 1] == actColor) { // Left
-                q.push({r, c - 1});
-                visited[r][c - 1] = true;
-                image[r][c - 1] = color;
-            }
-            if (c + 1 < image[0].size() && !visited[r][c + 1] && image[r][c + 1] == actColor) { // Right
-                q.push({r, c + 1});
-                visited[r][c + 1] = true;
-                image[r][c + 1] = color;
-            }
+
 
         }
         return image;
-
         
     }
 };
