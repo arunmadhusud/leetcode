9cbf85c674042b06232eedcba45b92a93f6fda03
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-12-17 23:43:55 -0500

    Time: 8 ms (67.34%) | Memory: 15.5 MB (41.03%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {        
-
-        int total_sum = 0;
-        int max_sum = 0;
-        for(int i = 0; i < nums.size(); i++){
-            total_sum += nums[i];
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum  = 0;
+        for (int i = 0; i < nums.size(); i++){
+            sum += nums[i];
         }
-        if ((total_sum - target) < 0 || (total_sum - target) % 2 != 0) {
+        int subset_sum = (sum - target) / 2 ;
+        if ((sum - target) < 0 || (sum - target) % 2 != 0) {
             return 0;
         }
-        int half_sum = (total_sum - target) / 2;
-        vector<vector<int>> dp(nums.size() + 1, vector<int>(half_sum+1, 0));
-
-        for(int i = 0; i <= nums.size(); i++) {
+        return subsetSum(nums, subset_sum);
+        
+    }
+    int subsetSum(vector<int>& nums, int sum){
+        std::vector<std::vector<int>> dp(nums.size()+1,std::vector<int>(sum+1,0));
+        for (int i = 0; i <= nums.size(); i++){
             dp[i][0] = 1;
         }
 
-        for(int i = 1; i <= nums.size(); i++) {
-            for (int j = 0; j <= half_sum; j++) {
-            if (nums[i-1] > j) {
-                dp[i][j] = dp[i-1][j];
-            } else {
-                dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i-1]];
-            }
+        for (int i = 1; i <= nums.size(); i++){
+            for (int j = 0; j <= sum; j++){
+                if (nums[i-1] > j) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
             }
         }
 
-        return (dp[nums.size()][half_sum]);
+        return dp[nums.size()][sum];
     }
 };
