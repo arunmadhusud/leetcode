fdd0cb736c52facad80e17baf735069e2b35c164
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2024-12-30 18:57:53 -0500

    Time: 23 ms (75.88%) | Memory: 88.5 MB (29.82%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        std::map<int,int> sum_idx;
-        // To handle the case where the array contains equal number of zeros and 1s
-        sum_idx[0] = -1;
+        std::unordered_map<int,int> sum_idx;
         int sum = 0;
-        int max_l = 0;
+        sum_idx[sum] = -1;
+        int max_length = 0;
         for (int i = 0; i < nums.size(); i++){
             if (nums[i]==0) sum--;
             else sum++;
             if(sum_idx.find(sum)!=sum_idx.end())
             {
-                max_l = max(max_l,i-sum_idx[sum]);
+                max_length = std::max(max_length, i - sum_idx[sum]);
             }
             else sum_idx[sum] = i;
         }
-        return max_l;
+        return max_length;
+
     }
 };
