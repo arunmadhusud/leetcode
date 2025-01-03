261ddd65cea6ba392442e617c091f76a8e0bcd51
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-01-03 14:56:51 -0500

    Time: 24 ms (70.72%) | Memory: 27.2 MB (49.71%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-
-        for(int i=1; i<=n; i++)  {
-            for(int j=1; j<=m; j++){
+        int m = text1.size();
+        int n = text2.size();
+        std::vector<std::vector<int>> dp(m+1,std::vector<int>(n+1,0));
+        for(int i = 1; i<= m; i++){
+            for (int j=1; j<=n; j++){
                 if(text1[i-1]==text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                else dp[i][j] = std::max(dp[i-1][j],dp[i][j-1]);
             }
-        } 
+        }        
+        return dp[m][n];
+        
+        
+    }
 
-        int x = n;
-        int y = m;
-        string lcsStr = "";
-
-        while(m>0 && n>0){
-            if(dp[n][m-1] ==dp[n][m]) m=m-1;
-            else{
-                lcsStr += text2[m-1];
-                m = m-1;
-                n=n-1;
-            }
-        }
-        reverse(lcsStr.begin(), lcsStr.end());
-        cout << "LCS: " << lcsStr << endl;
-
-        return dp[x][y];  
-    }   
-
+    int lcs(string text1, string text2,int m,int n,std::vector<std::vector<int>>& dp ){        
+        if (m==0 || n==0) return 0;
+        if (dp[m][n]!=-1) return dp[m][n];
+        else if (text1[m-1]==text2[n-1]) dp[m][n] = 1 + lcs(text1, text2,m-1,n-1,dp);
+        else dp[m][n] = max(lcs(text1, text2,m-1,n,dp),lcs(text1, text2,m,n-1,dp));
+        return dp[m][n];
+    }
 };
