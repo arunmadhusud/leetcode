d1018454a6783030a1e5e459a4bfd89ca55be26e
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-01-06 10:42:59 -0500

    Time: 1319 ms (49.90%) | Memory: 65.5 MB (5.01%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     int minCut(string s) {
         int n = s.size();
-        // Memoization table for the recursion
-        std::vector<std::vector<int>> dp(n, std::vector<int>(n, -1));
-        // Precompute palindrome checks
-        std::vector<std::vector<bool>> isPal(n, std::vector<bool>(n, false));
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                isPal[i][j] = checkPalindrome(s, i, j);
+        int i = 0;
+        int j = n-1;
+        std::vector<std::vector<int>> dp(n,std::vector<int>(n,-1));
+        std::vector<std::vector<bool>> isPal(n,std::vector<bool>(n,false));
+        for (int i = 0; i< n; i++){
+            for (int j = i; j<n; j++){
+                if(isPalindrome(s,i,j)) isPal[i][j] = true;
             }
         }
-        return mc(s, 0, n - 1, dp, isPal);
+        return mc(s,i,j,dp,isPal);
+        
     }
-
-    int mc(string& s, int i, int j, std::vector<std::vector<int>>& dp, std::vector<std::vector<bool>>& isPal) {
-        if (dp[i][j] != -1) return dp[i][j];
-        if (i >= j || isPal[i][j]) return 0; // No cuts needed if already a palindrome
-
+    int mc(string& s, int i, int j, std::vector<std::vector<int>>& dp, std::vector<std::vector<bool>>& isPal){
+        if (dp[i][j]!=-1) return dp[i][j];
+        if (i>=j || isPal[i][j]) return 0;
         int minCut = INT_MAX;
+        int cuts;
         for (int k = i; k < j; k++) {
             if (isPal[i][k]) { // Only make a cut if the left part is a palindrome
                 int cuts = 1 + mc(s, k + 1, j, dp, isPal);
                 minCut = min(minCut, cuts);
             }
         }
-
         dp[i][j] = minCut;
         return dp[i][j];
+
     }
 
-    bool checkPalindrome(string& s, int i, int j) {
+    bool isPalindrome(string& s, int i, int j){
         while (i < j) {
             if (s[i] != s[j]) return false;
             i++;
