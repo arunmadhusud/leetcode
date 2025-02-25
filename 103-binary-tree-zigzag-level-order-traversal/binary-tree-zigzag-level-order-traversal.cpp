159412e0c0b37e97cbec9c7db7944fd389d3238b
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-02-25 00:24:14 -0500

    Time: 1 ms (17.03%) | Memory: 15.2 MB (18.18%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -12,32 +12,25 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if (root==nullptr) return {};
         vector<vector<int>> result;
-        if (root==nullptr) return result;
-        queue<TreeNode*> q;
+        std::queue<TreeNode*> q;
         q.push(root);
-        int flag = 0;
+        int level;        
         while(!q.empty()){
-            int k = q.size();
-            vector<int> currLevel;
-            for (int i = 0; i < k; i++){
+            level +=1;
+            std::vector<int> tmp;
+            int k =  q.size();
+            for (int i=0; i< k; i++){
                 TreeNode* curr = q.front();
-                currLevel.push_back(curr->val);
                 q.pop();
-                if(curr->left !=nullptr) q.push(curr->left);
-                if(curr->right !=nullptr) q.push(curr->right);
+                if(curr->left != nullptr) q.push(curr->left);
+                if(curr->right != nullptr) q.push(curr->right);
+                tmp.push_back(curr->val);
             }
-            if (flag==0){
-                result.push_back(currLevel);
-                flag = 1;
-            }
-            else{
-                reverse(currLevel.begin(),currLevel.end());
-                result.push_back(currLevel);
-                flag = 0;
-            }
+            if (level % 2 == 0) std::reverse(tmp.begin(),tmp.end());
+            result.push_back(tmp);
         }
         return result;
-        
     }
 };
