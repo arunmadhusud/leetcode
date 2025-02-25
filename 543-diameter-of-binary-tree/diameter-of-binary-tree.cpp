7fd4fa0a40574c18bbf8dbd01ec9affe89ab00bf
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-02-24 23:39:38 -0500

    Time: 0 ms (100.00%) | Memory: 23.5 MB (92.76%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -17,13 +17,10 @@ public:
         return max_dia;      
     }
     int maxDepth(TreeNode* root, int& max_dia) {
-        int dia = 0;
-        if (root==nullptr) return  -1;
+        if (root==nullptr) return  0;
         int leftHeight =  maxDepth(root->left,max_dia);
-        if (leftHeight >= 0) dia = dia + leftHeight + 1;
         int rightHeight = maxDepth(root->right,max_dia);
-        if (rightHeight >= 0) dia = dia + rightHeight + 1;
-        max_dia = max(max_dia,dia);
+        max_dia = max(max_dia,leftHeight+rightHeight);
         return max(leftHeight,rightHeight) + 1;        
     }
 
