5b70c75f620324d44d5682c111e8c9a6433c7cba
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-02-25 23:05:18 -0500

    Time: 3 ms (29.65%) | Memory: 17.7 MB (52.88%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -9,29 +9,50 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<string> binaryTreePaths(TreeNode* root) {
+//         std::vector<int> pathVec;
+//         vector<string> paths;
+//         if (!root) return {};
+//         pathUtil(root,pathVec,paths);
+//         return paths;
+//     }
+
+//     void pathUtil(TreeNode* root,std::vector<int>& pathVec,vector<string>& paths){  
+//         if (!root) return;
+//         pathVec.push_back(root->val);      
+//         if (!(root->left) && !(root->right)){
+//             string path = "";
+//             for (size_t i = 0; i < pathVec.size(); i++) {
+//                 path += std::to_string(pathVec[i]);
+//                 if (i != pathVec.size() - 1) path += "->"; 
+//             }
+//             paths.push_back(path);
+//         }
+//         if ((root->left))  pathUtil(root->left,pathVec,paths);
+//         if ((root->right)) pathUtil(root->right,pathVec,paths);
+//         pathVec.pop_back();
+//     }
+// };
+
+
 class Solution {
 public:
-    vector<string> binaryTreePaths(TreeNode* root) {
-        std::vector<int> pathVec;
-        vector<string> paths;
-        if (!root) return {};
-        pathUtil(root,pathVec,paths);
-        return paths;
+    void dfs(TreeNode* root, vector<string>& result, string temp){
+        if(root==nullptr) return;
+        temp += std::to_string(root->val);
+        if (!(root->left) && !(root->right)) result.push_back(temp);
+        else{
+            temp +="->";
+            dfs(root->left,result,temp);
+            dfs(root->right,result,temp);
+        }
     }
 
-    void pathUtil(TreeNode* root,std::vector<int>& pathVec,vector<string>& paths){  
-        if (!root) return;
-        pathVec.push_back(root->val);      
-        if (!(root->left) && !(root->right)){
-            string path = "";
-            for (size_t i = 0; i < pathVec.size(); i++) {
-                path += std::to_string(pathVec[i]);
-                if (i != pathVec.size() - 1) path += "->"; 
-            }
-            paths.push_back(path);
-        }
-        if ((root->left))  pathUtil(root->left,pathVec,paths);
-        if ((root->right)) pathUtil(root->right,pathVec,paths);
-        pathVec.pop_back();
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> result;
+        dfs(root, result, "");
+        return result;
     }
 };
