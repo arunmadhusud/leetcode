0a9f69e054a1e909d1371a6659bcaa5c802e3852
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-02-26 01:27:16 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (68.76%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -17,22 +17,21 @@ public:
         std::queue<std::pair<TreeNode*,long>> q;
         q.push({root,0});        
         while(!q.empty()){
-            std::vector<long> tmp;
             int k =  q.size();
-            long min_idx;;
+            long min_idx = q.front().second;
+            long first = q.front().second;
+            long last = q.back().second;
+            int width = static_cast<int>(last-first) + 1;
+            max_width = max(width,max_width);
             for (int i=0; i< k; i++){
                 auto curr = q.front();
                 TreeNode* currNode = curr.first;
                 long idx = curr.second;
-                if (i==0) min_idx = idx;
                 idx -=min_idx;    
                 q.pop();
                 if(currNode->left != nullptr) q.push({currNode->left, 2*idx+1});
                 if(currNode->right != nullptr) q.push({currNode->right, 2*idx+2});
-                tmp.push_back(idx);
             }
-            int width = tmp[tmp.size()-1] + 1;
-            max_width = max(width,max_width);
         }
         return max_width;
         
