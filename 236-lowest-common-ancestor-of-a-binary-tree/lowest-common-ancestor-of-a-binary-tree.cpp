747417fb8f3e19c404d4d9d22d0b46350f784a47
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-02-26 00:24:28 -0500

    Time: 11 ms (67.33%) | Memory: 18.9 MB (6.06%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,17 +10,25 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || root == p || root == q ) return root;
-        TreeNode* leftNode = lowestCommonAncestor(root->left,p,q);
-        TreeNode* rightNode = lowestCommonAncestor(root->right,p,q); 
-
-        if(!leftNode) return rightNode;
-        if(!rightNode) return leftNode;
-        return root;
-
-
+        vector<TreeNode*> p_result;
+        vector<TreeNode*> q_result;
+        TreeNode* result = nullptr;
+        lca(root,p,p_result);
+        lca(root,q,q_result);
+        size_t min_size = std::min(p_result.size(),q_result.size());
+        for(size_t i = 0 ; i < min_size; i++){
+            if (p_result[i]!=q_result[i]) break;
+            result = p_result[i];
+        }
+        return result;        
     }
 
-
-    
+    bool lca(TreeNode* root,TreeNode* p, std::vector<TreeNode*>& result){
+        if (!root) return false;
+        result.push_back(root);
+        if (root==p) return true;       
+        if (lca(root->left,p,result) || lca(root->right,p,result)) return true;
+        result.pop_back();
+        return false;
+    }
 };
