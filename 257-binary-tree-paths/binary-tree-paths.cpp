0296f07abf68857f7058002d072d4ca0e4694579
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-02-25 22:53:50 -0500

    Time: 0 ms (100.00%) | Memory: 17.4 MB (81.92%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        std::vector<int> pathVec;
+        vector<string> paths;
+        if (!root) return {};
+        pathUtil(root,pathVec,paths);
+        return paths;
+    }
+
+    void pathUtil(TreeNode* root,std::vector<int>& pathVec,vector<string>& paths){  
+        if (!root) return;
+        pathVec.push_back(root->val);      
+        if (!(root->left) && !(root->right)){
+            string path = "";
+            for (size_t i = 0; i < pathVec.size(); i++) {
+                path += std::to_string(pathVec[i]);
+                if (i != pathVec.size() - 1) path += "->"; 
+            }
+            paths.push_back(path);
+        }
+        if ((root->left))  pathUtil(root->left,pathVec,paths);
+        if ((root->right)) pathUtil(root->right,pathVec,paths);
+        if(pathVec.size()!=1) pathVec.pop_back();
+    }
+};
