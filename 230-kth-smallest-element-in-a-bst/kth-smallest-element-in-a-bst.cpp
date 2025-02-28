105deaebc02c5b9b8f9a188b70979bb25dd1bc8f
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-02-27 21:02:44 -0500

    Time: 0 ms (100.00%) | Memory: 24.4 MB (42.63%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -9,23 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int kthSmallest(TreeNode* root, int k) {
+//         std::priority_queue<int> pq;
+//         for ( int i = 0; i<k; i++) pq.push(INT_MAX);
+//         dfs(root,pq,k);
+//         return pq.top();
+        
+//     }
+//     void dfs(TreeNode* root,std::priority_queue<int>& pq,int& k ){
+//         if (!root) return;
+//         if (k < pq.size()) pq.push(root->val);
+//         else if (root->val < pq.top()) {
+//             pq.pop();
+//             pq.push(root->val);
+//         }
+//         dfs(root->left,pq,k);
+//         dfs(root->right,pq,k);
+//     }
+// };
+
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
-        std::priority_queue<int> pq;
-        for ( int i = 0; i<k; i++) pq.push(INT_MAX);
-        dfs(root,pq,k);
-        return pq.top();
-        
+        int elem;
+        dfs(root, elem, k);
+        return elem;      
+
     }
-    void dfs(TreeNode* root,std::priority_queue<int>& pq,int& k ){
-        if (!root) return;
-        if (k < pq.size()) pq.push(root->val);
-        else if (root->val < pq.top()) {
-            pq.pop();
-            pq.push(root->val);
-        }
-        dfs(root->left,pq,k);
-        dfs(root->right,pq,k);
+    void dfs(TreeNode* root, int& elem, int& k){
+        if(!root) return;        
+        dfs(root->left,elem,k);
+        k--;        
+        if (k==0){
+            elem = root->val;
+            return;
+        } 
+        dfs(root->right,elem,k);        
     }
 };
