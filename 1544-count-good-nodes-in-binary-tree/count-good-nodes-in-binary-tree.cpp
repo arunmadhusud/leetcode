ab5f786f1fa93a68cc802247872361b01698be88
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-02-27 19:46:55 -0500

    Time: 406 ms (5.77%) | Memory: 365.5 MB (5.46%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -16,19 +16,18 @@ public:
         int count = 0;
         dfs(root, pathVec, count );
         return count;        
-    }
-    
+    }    
     int findMax(std::vector<int>& pathVec){
         int max_elem = INT_MIN;
         for (auto elem : pathVec) max_elem = max(max_elem,elem);
         return max_elem;
     }
-    void dfs(TreeNode* root, std::vector<int>& pathVec, int& count ){
+    void dfs(TreeNode* root, std::vector<int> pathVec, int& count ){
         if(!root) return;
         pathVec.push_back(root->val);
         if (root->val >= findMax(pathVec)) count++;
         dfs(root->left,pathVec,count);
         dfs(root->right,pathVec,count);
-        pathVec.pop_back();
+        // pathVec.pop_back();
     }
 };
