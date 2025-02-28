f4cb4b26ce6519f2377d64657054bdc1da89498a
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-02-27 19:55:16 -0500

    Time: 99 ms (54.49%) | Memory: 88.3 MB (61.20%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -12,22 +12,32 @@
 class Solution {
 public:
     int goodNodes(TreeNode* root) {
-        std::vector<int> pathVec;
+        // std::vector<int> pathVec;
         int count = 0;
-        dfs(root, pathVec, count );
+        int max_elem = INT_MIN;
+        dfs(root, max_elem, count );
         return count;        
     }    
-    int findMax(std::vector<int>& pathVec){
-        int max_elem = INT_MIN;
-        for (auto elem : pathVec) max_elem = max(max_elem,elem);
-        return max_elem;
-    }
-    void dfs(TreeNode* root, std::vector<int>& pathVec, int& count ){
+    // int findMax(std::vector<int>& pathVec){
+    //     int max_elem = INT_MIN;
+    //     for (auto elem : pathVec) max_elem = max(max_elem,elem);
+    //     return max_elem;
+    // }
+    // void dfs(TreeNode* root, std::vector<int>& pathVec, int& count ){
+    //     if(!root) return;
+    //     pathVec.push_back(root->val);
+    //     if (root->val >= findMax(pathVec)) count++;
+    //     dfs(root->left,pathVec,count);
+    //     dfs(root->right,pathVec,count);
+    //     pathVec.pop_back();
+    // }
+
+    void dfs(TreeNode* root, int max_elem, int& count ){
         if(!root) return;
-        pathVec.push_back(root->val);
-        if (root->val >= findMax(pathVec)) count++;
-        dfs(root->left,pathVec,count);
-        dfs(root->right,pathVec,count);
-        pathVec.pop_back();
+        if (root->val >= max_elem) count++;
+        max_elem = max(root->val,max_elem);
+        dfs(root->left,max_elem ,count);
+        dfs(root->right,max_elem ,count);
+        // pathVec.pop_back();
     }
 };
