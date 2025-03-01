72bc5509d7d07e57f1a5cff4162fdd7934da522f
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-03-01 17:46:26 -0500

    Time: 3 ms (73.68%) | Memory: 15.6 MB (10.80%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> indices;
-        for (int i = 0; i< nums.size(); i++)
-        {
-            int num_diff = target - nums[i];
-            auto it_vec = std::find(nums.begin() + i + 1, nums.end(), num_diff);
-            if (it_vec!= nums.end())
-            {
-                indices.push_back(i);
-                indices.push_back(it_vec - nums.begin() );
-                return indices;
+        std::unordered_set<int> diff;
+        std::unordered_map<int,int> mp;
+        std::vector<int> result;
+        for(int i = 0; i< nums.size(); i++){
+            int d = target-nums[i];
+            if(diff.find(d)!=diff.end()){
+                result.push_back(i);
+                result.push_back(mp[d]);
+                return result;
+            }
+            else {
+                diff.insert(nums[i]);
+                mp[nums[i]] = i;
             }
         }
-    return indices;
+        return result;
     }
 };
