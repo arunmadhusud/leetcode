20b85b1f10beea685ff14b80b06b2e4f564dddfc
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-03-01 18:12:16 -0500

    Time: 17 ms (58.78%) | Memory: 26 MB (48.19%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
-        std::map<std::string, std::vector<std::string>> anagram_map;
-        std::vector<std::vector<std::string>> result;
-
-        // Group words by their sorted form
-        for (int i = 0; i < strs.size(); ++i) {
-            std::string str_copy = strs[i];
-            std::sort(str_copy.begin(), str_copy.end()); // Sort the string to use as a key
-            anagram_map[str_copy].push_back(strs[i]);
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::unordered_map<std::string,vector<string>> mp;
+        vector<vector<string>> result;
+        for(int i =0; i < strs.size(); i++){
+            std::string str_cp(strs[i]);
+            std::sort(str_cp.begin(),str_cp.end());
+            mp[str_cp].push_back(strs[i]);
         }
 
-        // Collect groups of anagrams
-        for (const auto& entry : anagram_map) {
-            result.push_back(entry.second); // Add the vector of anagrams to the result
+        for(auto elem:mp){
+            result.push_back(elem.second);
         }
 
         return result;
+        
     }
 };
