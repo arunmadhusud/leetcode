83aecf4d4088fecc7393a0ec236ce8dc36264d8a
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-03-02 17:26:57 -0500

    Time: 115 ms (18.84%) | Memory: 90 MB (14.61%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,28 +1,52 @@
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         std::priority_queue<int,vector<int>, std::greater<int>> pq;
+//         for(int i = 0; i< nums.size(); i++) pq.push(nums[i]);
+//         int count = 0;
+//         int max_count = 0;
+//         int prev,curr;
+//         if(pq.size() > 0)
+//         {
+//             prev = pq.top();
+//             pq.pop();
+//             count = 1;
+//             max_count = 1;
+//         }
+//         while(!pq.empty()){
+//             curr = pq.top();
+//             if ((curr-prev) > 1) count = 1;
+//             else if(curr!=prev) count++;
+//             prev = curr;
+//             pq.pop();
+//             max_count = max(count,max_count);            
+//         }
+
+//         return max_count;
+        
+//     }
+// };
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        std::priority_queue<int,vector<int>, std::greater<int>> pq;
-        for(int i = 0; i< nums.size(); i++) pq.push(nums[i]);
+        set<int> numSet(nums.begin(), nums.end());
         int count = 0;
         int max_count = 0;
         int prev,curr;
-        if(pq.size() > 0)
-        {
-            prev = pq.top();
-            pq.pop();
-            count = 1;
-            max_count = 1;
-        }
-        while(!pq.empty()){
-            curr = pq.top();
+        for (auto it = numSet.begin(); it !=numSet.end(); it++){
+            curr = *it;
+            if (it == numSet.begin()){
+                prev = curr;
+                count = 1;
+                max_count = 1; 
+                continue;               
+            }
             if ((curr-prev) > 1) count = 1;
             else if(curr!=prev) count++;
             prev = curr;
-            pq.pop();
-            max_count = max(count,max_count);            
+            max_count = max(count,max_count);
         }
-
         return max_count;
-        
     }
 };
