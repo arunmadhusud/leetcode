e351aa893d43da3479f5585f85c64f3db27f6b80
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-03-01 20:17:01 -0500

    Time: 19 ms (5.27%) | Memory: 18 MB (31.29%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        map<int,int> num_count;
-        for (int i = 0; i < nums.size(); i++) {
-            num_count[nums[i]]++;  // Count frequency of each number
-        }
+        std::map<int,int> mp;
         priority_queue<pair<int,int>> pq;
         vector<int> result;
-        for (pair<int,int> elem : num_count)
-        {
-            pq.push({elem.second,elem.first});
+        for(int i =0; i< nums.size(); i++){
+            mp[nums[i]]++;
         }
-        while(k-- && !pq.empty())
-        {
+        for (const auto &elem : mp) {
+            std::cout << elem.first << " -> " << elem.second << std::endl;
+        }
+        for(auto it=mp.begin();it!=mp.end();it++){
+            pq.push({it->second,it->first});
+        }
+
+        for(int i =0; i<k && (!pq.empty()); i++){
             result.push_back(pq.top().second);
             pq.pop();
+
         }
+
         return result;
- 
+        
     }
 };
