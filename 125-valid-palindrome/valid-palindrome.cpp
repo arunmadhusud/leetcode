dd2a53d72970575119964805d5d23f48fee20035
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-03-02 22:09:40 -0500

    Time: 3 ms (32.27%) | Memory: 9.7 MB (93.91%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string s_new="";
-        for(char ch:s){
-            if(isalnum(ch)){
-                s_new+=tolower(ch);
-            } 
-        }
         int i = 0;
         int j = s.size()-1;
         while(i<j){
