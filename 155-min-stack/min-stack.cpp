7c0b726ed43cc98a8127cbced626d0d9b07f9805
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-03-03 18:25:10 -0500

    Time: 3 ms (62.46%) | Memory: 23.4 MB (53.11%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,40 +1,31 @@
 class MinStack {
 public:
-    // Stack to store pairs of (value, min_value_at_that_point)
-    stack<pair<int, int>> st;
-
-    MinStack() {}
-
+    std::stack<pair<int,int>> st;
+    int min_elem;
+    MinStack() { 
+        min_elem = INT_MAX;    
+    }
+    
     void push(int val) {
-        if (st.empty()) {
-            // If stack is empty, initialize the min to the value being pushed
-            st.push({val, val});
-        } else {
-            // Get the current minimum value from the top of the stack
-            int current_min = st.top().second;
-            // Push the new value with the minimum of the current value and the previous minimum
-            st.push({val, min(val, current_min)});
-        }
+        if (st.empty()) min_elem = val;
+        else min_elem = min(st.top().second, val);
+        st.push({val, min_elem});      
     }
     
     void pop() {
-        if (!st.empty()) {
-            st.pop();
-        }
+        if(!st.empty()) st.pop();
+      
+        
     }
     
     int top() {
-        if (!st.empty()) {
-            return st.top().first;
-        }
-        throw runtime_error("Stack is empty");
+        if(!st.empty()) return st.top().first;
+        else return -1;        
     }
     
     int getMin() {
-        if (!st.empty()) {
-            return st.top().second;
-        }
-        throw runtime_error("Stack is empty");
+        return st.top().second;
+        
     }
 };
 
