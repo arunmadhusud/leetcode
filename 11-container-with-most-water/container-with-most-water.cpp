0bdb14fd4fe403028a30db4eb46095c7b918cdd1
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-03-03 00:01:26 -0500

    Time: 1 ms (46.62%) | Memory: 63 MB (11.56%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,30 +1,16 @@
-#include <vector>
-#include <algorithm> // for std::max
-
 class Solution {
 public:
-    int maxArea(std::vector<int>& height) {
+    int maxArea(vector<int>& height) {
+        int max_area = 0;
         int i = 0;
-        int j = height.size() - 1;
-        int max_area = 0; // To keep track of the maximum area
-
-        while (i < j) {
-            // Calculate the current area
-            int width = j - i;
-            int height_min = std::min(height[i], height[j]);
-            int area = width * height_min;
-
-            // Update the maximum area
-            max_area = std::max(max_area, area);
-
-            // Move the pointer that points to the shorter line
-            if (height[i] < height[j]) {
-                ++i;
-            } else {
-                --j;
-            }
+        int j = height.size()-1;
+        while(i <j){
+            int area = min(height[i],height[j])*(j-i);
+            max_area = max(area,max_area);
+            if (height[i]<height[j]) i++;
+            else j--;
         }
-
         return max_area;
+        
     }
 };
