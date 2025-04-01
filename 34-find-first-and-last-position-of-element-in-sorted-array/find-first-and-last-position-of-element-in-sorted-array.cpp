2cc17ec32da908cc91830e16ba33bb67d6556ba0
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-01 19:27:59 -0400

    Time: 0 ms (100.00%) | Memory: 17.4 MB (86.80%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,30 @@
 class Solution {
 public:
+    int findOccur(vector<int>& nums, int target, bool firstOccur){
+        int low = 0;
+        int high = nums.size() - 1;
+        int result = -1;
+        while (low <= high){
+            int mid  = low + (high - low)/2;
+            if (nums[mid] == target) {
+                result = mid;
+                if (firstOccur) high = mid -1;
+                else low = mid + 1;
+
+            }
+            else if (nums[mid] > target) high = mid - 1;
+            else low = mid+1;
+        }
+
+        return  result;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> result;
-        int first_occ = firstOccur(nums, target);
-        int last_occ = lastOccur(nums, target);
-        result.push_back(first_occ);
-        result.push_back(last_occ);
-        return result;
+        int firstIdx = findOccur(nums, target, true);
+        int lastIdx = findOccur(nums, target, false);
+        result.push_back(firstIdx);
+        result.push_back(lastIdx);
+        return result;        
+    }
         
-    }
-    int lastOccur(vector<int>& a, int target)
-    {
-        int start = 0;
-        int end = a.size()-1;
-        int result = -1;
-        while (start <= end)
-        {
-            int mid = start + (end-start)/2;
-            if (a[mid] < target) start = mid + 1;
-            if (a[mid] > target) end = mid - 1;
-            if (a[mid]==target)
-            {
-                result = mid;
-                start = mid + 1;
-            }
-        }
-        return result;
-    }
-    int firstOccur(vector<int>& a, int target)
-    {
-        int start = 0;
-        int end = a.size()-1;
-        int result = -1;
-        while (start <= end)
-        {
-            int mid = start + (end-start)/2;
-            if (a[mid] < target) start = mid + 1;
-            if (a[mid] > target) end = mid - 1;
-            if (a[mid]==target)
-            {
-                result = mid;
-                end = mid - 1;
-            }
-        }
-        return result;
-    }
 };
