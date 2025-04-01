71c4fd566deb7a2e7b4bf5255b26094d789f66e7
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-01 12:56:44 -0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (68.95%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result(nums1.size(),-1);
-        stack<int> stIdx;
-        unordered_map<int,int> grtMP;
-        
-        for(int i = 0; i < nums2.size(); i++){
-            while(!stIdx.empty() && nums2[stIdx.top()] < nums2[i]){
-                grtMP[nums2[stIdx.top()]] = nums2[i];
-                stIdx.pop();
+        stack<int> mon_st;
+        unordered_map<int, int> nge_map;
+        vector<int> res(nums1.size(),-1);
+
+        for(int i = nums2.size()-1 ; i >=0 ; i--){
+            while (!mon_st.empty() && mon_st.top() <= nums2[i]){
+                mon_st.pop();
             }
-            stIdx.push(i);
+            nge_map[nums2[i]] = mon_st.empty() ? -1 : mon_st.top();
+            mon_st.push(nums2[i]);
+        }
+        for (int i = 0; i < nums1.size() ; i++)
+        {
+            res[i] = nge_map[nums1[i]];
         }
 
-        for(int i = 0; i < nums1.size(); i++){
-            if(grtMP.find(nums1[i])!=grtMP.end()) result[i] = grtMP[nums1[i]];
-        }
-        return result;
+        return res;
         
     }
 };
