110ccaa9deedbb9c2b1f61b2298250396b934010
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-05 14:50:20 -0400

    Time: 0 ms (100.00%) | Memory: 27.2 MB (30.78%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
-    // int findMaxleft(vector<int>& height, int i) {
-    //     int m = 0;  
-    //     for(int j = 0; j < i; j++) {
-    //         m = max(m, height[j]);
-    //     }
-    //     return m;
-    // }
-
-    // int findMaxright(vector<int>& height, int i) {
-    //     int m = 0;
-    //     for(int j = i + 1; j < height.size(); j++) {
-    //         m = max(m, height[j]);
-    //     }
-    //     return m;
-    // }
-
     int trap(vector<int>& height) {
-        if(height.size() < 3) return 0; 
-        vector<int> prefixMax(height.size(),0);
-        vector<int> suffixMax(height.size(),0);
-        prefixMax[0] = height[0];
-        // for(int i = 1; i<height.size(); i++){
-        //     prefixMax[i] = max(prefixMax[i-1],height[i]);         
-        // }
-        suffixMax[height.size()-1] = height[height.size()-1];
-        for(int j = height.size()-2; j>=0; j--){
-            suffixMax[j] = max(suffixMax[j+1],height[j]);         
+        int n = height.size();
+        if (n < 3) return 0;
+        vector<int> leftMax(n, 0);
+        vector<int> rightMax(n, 0);
+        for (int i = 1 ; i < n; i++){
+            leftMax[i] = max(leftMax[i-1],height[i-1]);
         }
+        for (int i = n-2 ; i >=0; i--){
+            rightMax[i] = max(rightMax[i+1],height[i+1]);
+        }
+
         int vol = 0;
-        int maxLeft = height[0];
-        for(int i = 1; i < height.size()-1; i++) {
-            // int 
-            // int maxLeft = prefixMax[i-1];
-            maxLeft = max(maxLeft,height[i]);
-            int maxRight = suffixMax[i+1];
-            int water = min(maxLeft, maxRight) - height[i];
-            if (water > 0) vol += water;
+        for (int i = 1;i < n-1; i++ ){
+            int min_h = min (leftMax[i], rightMax[i]);
+            vol += max(0, min_h - height[i]);
         }
+
         return vol;
+
+        
     }
 };
