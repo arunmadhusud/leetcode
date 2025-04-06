c5d04c2f552b0180135b5eaf8b79f8b342dffd2a
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-05 22:39:10 -0400

    Time: 0 ms (100.00%) | Memory: 12.9 MB (47.63%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,7 +15,8 @@ public:
         if(p==nullptr && q==nullptr) return true;
         if ((!p && q) || (p && !q)) return false;
         if (p->val != q->val) return false;
-        return (isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
-        
+        if (!isSameTree(p->left,q->left)) return false;
+        if (!isSameTree(p->right,q->right)) return false;
+        return true;
     }
 };
