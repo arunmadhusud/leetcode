b9860d162bb63e878ad8fa29f92f2b4c54c15bf7
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-08 13:43:13 -0400

    Time: 9 ms (38.74%) | Memory: 10.8 MB (25.53%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int l = 0;
-        int r = s1.size()-1;
+        int r = 0;
+        int window_size = s1.size();
         std::unordered_map<char,int> freq_mp;
         std::unordered_map<char,int> window_freq_mp;
         for (int i = 0; i < s1.size(); i++) freq_mp[s1[i]]++;
 
         while(r<s2.size()){
-            for (int i = l; i <= r; i++) window_freq_mp[s2[i]]++;
+            window_freq_mp[s2[r]]++;
+            if (r >= window_size) {
+                char left_char = s2[l];
+                window_freq_mp[left_char]--;
+                if (window_freq_mp[left_char] == 0) {
+                    window_freq_mp.erase(left_char);
+                }
+                l++;
+            }
             if(window_freq_mp==freq_mp) return true;
-            window_freq_mp = {};
-            l++;
             r++;
         }
         return false;
