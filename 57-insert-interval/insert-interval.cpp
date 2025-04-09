9c4eda13e87cc7d4276bfb8d2a5e00fe896f39ce
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-09 11:59:26 -0400

    Time: 0 ms (100.00%) | Memory: 21.8 MB (20.98%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,88 @@
+// class Solution {
+// public:
+//     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+//         int low = 0;
+//         int high = intervals.size()-1;
+//         int mid;
+//         vector<vector<int>> result;
+//         if (intervals.size()==0) {
+//             result.push_back(newInterval);
+//             return result;
+//         }
+//         while (low <=high){
+//             mid = low  + (high-low)/2;
+//             if (intervals[mid][0] > newInterval[0]) high = mid -1;
+//             else if (intervals[mid][1] < newInterval[0]) low = mid + 1;
+//             else break;
+//         }
+//         int merge_idx;
+//         int merge_val;
+//         for (int i = mid; i < intervals.size(); i++){
+
+//             if (i == mid && intervals[i][1]>=newInterval[1]){
+//                 merge_idx = i;
+//                 merge_val = intervals[i][1];
+//                 break;
+//             }
+//             // else if (intervals[i][1] < newInterval[1] && intervals[i+1][0] > newInterval[1]){
+//             //     merge_idx = i;
+//             //     merge_val = newInterval[1];
+//             //     break;
+//             // }
+//             else if (intervals[i][1] > newInterval[1]){
+//                 merge_idx = i;
+//                 merge_val = intervals[i][1] ;
+//                 break;
+//             }
+//             else if (intervals[i][0] >= newInterval[1]){
+//                 merge_idx = i-1;
+//                 merge_val = newInterval[1] ;
+//                 break;
+//             }
+//         }
+
+//         for  (int i = 0; i<mid; i++) result.push_back(intervals[i]);
+//         vector<int> tmp;
+//         tmp.push_back(intervals[mid][0]);
+//         tmp.push_back(merge_val);
+//         result.push_back(tmp);
+//         for (int i = merge_idx+1;i< intervals.size(); i++) result.push_back(intervals[i]);
+        
+//         return result;
+
+        
+//     }
+// };
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> result;
+        int n = intervals.size();
+        int i = 0;
+        
+        // Add all intervals that come before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+        
+        // Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        
+        // Add the merged interval
+        result.push_back(newInterval);
+        
+        // Add all intervals that come after newInterval
+        while (i < n) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+        
+        return result;
+    }
+};
