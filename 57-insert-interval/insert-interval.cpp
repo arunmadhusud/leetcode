a76ab544e4fe25d3db14b09dae39ad6ba0463d5d
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-09 12:23:25 -0400

    Time: 4 ms (19.64%) | Memory: 21.7 MB (46.36%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -60,29 +60,30 @@ public:
         vector<vector<int>> result;
         int n = intervals.size();
         int i = 0;
-        
-        // Add all intervals that come before newInterval
-        while (i < n && intervals[i][1] < newInterval[0]) {
+
+        if (intervals.size()==0) {
+            result.push_back(newInterval);
+            return result;
+        }
+
+        while(i < n && intervals[i][1] < newInterval[0]){
             result.push_back(intervals[i]);
             i++;
         }
-        
-        // Merge overlapping intervals
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
+
+        while(i < n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = min(newInterval[0],intervals[i][0]);
+            newInterval[1] = max(newInterval[1],intervals[i][1]);
             i++;
         }
-        
-        // Add the merged interval
         result.push_back(newInterval);
-        
-        // Add all intervals that come after newInterval
-        while (i < n) {
+
+        while(i < n){
             result.push_back(intervals[i]);
             i++;
         }
         
+
         return result;
     }
 };
