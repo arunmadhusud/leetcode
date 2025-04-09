09df3f8583b8ca28fa36cac060177d2ae5892e1f
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-09 01:51:46 -0400

    Time: 14 ms (31.12%) | Memory: 55.8 MB (63.05%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -14,17 +14,26 @@ public:
     }
     
     vector<int> getNewsFeed(int userId) {
-        std::priority_queue<pair<int,int>> pq;
+        std::priority_queue<pair<int, int>, std::vector<pair<int, int>>, std::greater<pair<int, int>>> pq; // Min heap
         std::vector<int> res;
         int k = 0;
-        for (const auto& elem : t_map[userId])pq.push(elem);
-        for (int f : f_map[userId]){
-            for (const auto& elem : t_map[f]) pq.push(elem);
+        for (const auto& tweet : t_map[userId]) {
+            pq.push(tweet); // Push tweet with timestamp
+            if (pq.size() > 10) pq.pop(); // Ensure heap size is capped at 10
+        }
+
+        // Add tweets of the users that the current user is following
+        for (int f : f_map[userId]) {
+            for (const auto& tweet : t_map[f]) {
+                pq.push(tweet); // Push tweet with timestamp
+                if (pq.size() > 10) pq.pop(); // Ensure heap size is capped at 10
+            }
         }
         while (!pq.empty() && res.size() <10){
             res.push_back(pq.top().second);
             pq.pop();
-        }        
+        }  
+        std::reverse(res.begin(), res.end());      
         return res;
     }
 
