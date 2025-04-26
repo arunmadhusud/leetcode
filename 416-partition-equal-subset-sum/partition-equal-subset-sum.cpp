23b7ed7a57147d4fd75bfe79c36c2b098c30f7a6
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-26 00:28:03 -0400

    Time: 79 ms (60.41%) | Memory: 13.4 MB (73.73%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -12,16 +12,28 @@ public:
 
         
     }
+    // bool isSubsetSum(vector<int>& arr, int sum) {
+    //     int n = arr.size();
+    //     vector<vector<bool>> dp(n+1,vector<bool>(sum+1,false));        
+    //     for (int  i = 0; i <= n ; i++) dp[i][0] = true;        
+    //     for (int i = 1; i <= n ; i++){
+    //         for(int j = 1; j <=sum; j++){
+    //             if (arr[i-1] > j) dp[i][j] = dp[i-1][j];
+    //             else dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+    //         }
+    //     }        
+    //     return dp[n][sum];
+    // }
+
     bool isSubsetSum(vector<int>& arr, int sum) {
-        int n = arr.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(sum+1,false));        
-        for (int  i = 0; i <= n ; i++) dp[i][0] = true;        
-        for (int i = 1; i <= n ; i++){
-            for(int j = 1; j <=sum; j++){
-                if (arr[i-1] > j) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+        vector<bool> dp(sum + 1, false);
+        dp[0] = true;     
+        for (int i = 1; i <= arr.size(); i++){
+            for(int j = sum; j >=arr[i-1]; j--){
+                dp[j] = dp[j] || dp[j-arr[i-1]];
             }
         }        
-        return dp[n][sum];
+        return dp[sum];
     }
+
 };
