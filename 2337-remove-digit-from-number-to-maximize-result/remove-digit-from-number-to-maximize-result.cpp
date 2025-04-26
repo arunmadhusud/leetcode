d375f4f8c08091219fbecde717934e84248f864d
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-26 14:22:36 -0400

    Time: 0 ms (100.00%) | Memory: 9.5 MB (18.61%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     string removeDigit(string number, char digit) {
-        int idx = -1;
-        for (int i = 0; i < number.size(); i++) {
-            if (number[i] == digit) {
-                idx = i;
-                if (i+1 < number.size() && number[i+1] > digit) {
-                    break; // better to remove earlier if next digit is bigger
-                }
+        std::vector<string> poss;
+        string tmp = "";
+        string max_s = "";
+        int length = number.size();
+        for(int i = 0; i < number.size(); i++){
+            if (number[i] == digit){
+                tmp += number.substr(0,i) + number.substr(i+1);
+                if(tmp > max_s) max_s = tmp;
+                tmp = "";
             }
         }
-        // remove the digit at idx
-        return number.substr(0, idx) + number.substr(idx+1);
+
+        return max_s;
+
+        
     }
 };
