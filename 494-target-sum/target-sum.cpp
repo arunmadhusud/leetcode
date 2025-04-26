259f2279879c00eb192318e01405322a486bd2b8
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-26 00:48:59 -0400

    Time: 7 ms (70.97%) | Memory: 15.4 MB (50.77%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum  = 0;
-        for (int i = 0; i < nums.size(); i++){
+        int sum = 0;
+        for (int i =0; i < nums.size(); i++){
             sum += nums[i];
         }
-        int subset_sum = (sum - target) / 2 ;
-        if ((sum - target) < 0 || (sum - target) % 2 != 0) {
-            return 0;
-        }
-        return subsetSum(nums, subset_sum);
+        if ((sum-target) < 0 || (sum-target)%2 !=0) return 0;
+        else return isSubsetSum(nums,(sum-target)/2);
         
     }
-    int subsetSum(vector<int>& nums, int sum){
-        std::vector<std::vector<int>> dp(nums.size()+1,std::vector<int>(sum+1,0));
-        for (int i = 0; i <= nums.size(); i++){
-            dp[i][0] = 1;
-        }
 
-        for (int i = 1; i <= nums.size(); i++){
-            for (int j = 0; j <= sum; j++){
-                if (nums[i-1] > j) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+    int isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,0));        
+        for (int  i = 0; i <= n ; i++) dp[i][0] = 1;        
+        for (int i = 1; i <= n ; i++){
+            for(int j = 0; j <=sum; j++){
+                if (arr[i-1] > j) dp[i][j] = dp[i-1][j];
+                else dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
             }
-        }
-
-        return dp[nums.size()][sum];
+        }        
+        return dp[n][sum];
     }
 };
