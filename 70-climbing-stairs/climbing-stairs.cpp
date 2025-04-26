3baabe6ec800ea487ebfe03b864a78b878e9967a
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-26 15:01:47 -0400

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n < 2) return 1;  // There is one way to climb 0 or 1 step
-
-        vector<int> table(2, 0); // Size of 2 to hold steps for last and second last step
-        table[0] = 1; // Ways to climb 0 steps
-        table[1] = 1; // Ways to climb 1 step
-        
-        // Use a variable to temporarily hold the previous value
-        int prev;
-        
-        // Start the loop from 2 to n (inclusive)
-        for (int i = 2; i <= n; i++) {
-            prev = table[1];                 // Store the second last step
-            table[1] = table[0] + table[1];  // Calculate current steps
-            table[0] = prev;                  // Update second last step
+        vector<int> dp(n+1);
+        if(n>=0) dp[0] = 1;
+        if(n>=1) dp[1] = 1;
+        for (int i = 2; i <=n; i++){
+            dp[i] = dp[i-2] + dp[i-1];
         }
+
+        return dp[n];
         
-        return table[1];  // Return the total ways to climb n steps
     }
 };
