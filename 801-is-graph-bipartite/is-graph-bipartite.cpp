f6434d2d38e4d0905d92f2f4ef40647ad0a5012e
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-27 16:44:36 -0400

    Time: 0 ms (100.00%) | Memory: 17.2 MB (80.28%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        std::vector<int> color(graph.size(),-1);
         std::queue<int> q;
-        int col = 0;
-        for (int i = 0; i<graph.size(); i++){ 
-            if (color[i] ==-1){
+        std::vector<int> bp(graph.size(),-1);
+        int c = 0;
+
+        for (int i = 0; i < graph.size(); i++ ){
+            if(bp[i] ==-1){
+                c = 0;
                 q.push(i);
-                color[i] = 0;
+                bp[i] = c;
                 while(!q.empty()){
                     int curr = q.front();
                     q.pop();
-                    for (int elem : graph[curr]){
-                        if(color[elem]==-1){
-                            color[elem] = 1-color[curr];
+                    for(int elem : graph[curr]){
+                        if(bp[elem]==-1){
                             q.push(elem);
+                            bp[elem] = 1 - bp[curr];
                         }
-                        else if(color[elem]==color[curr]) return false;                        
+                        else if (bp[elem]!= -1 && bp[elem]==bp[curr]) return false;
                     }
                 }
-
             }
         }
+
         return true;
+        
     }
 };
