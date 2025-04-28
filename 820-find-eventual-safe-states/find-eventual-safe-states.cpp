b93eef47bf3f936deec664c2f0006b81467a9bcf
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-04-27 20:55:31 -0400

    Time: 45 ms (56.92%) | Memory: 66.6 MB (12.33%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
-        int V = adj.size();
-        vector<bool> visited(V, false);
-        vector<bool> dfs_path(V, false);
-        vector<int>  check(V, -1);
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                dfs(i, visited, dfs_path, adj, check);
-            }
-        }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> result;
         vector<int> safeNodes;
-        for (int i = 0; i < V; i++) {
-            if (check[i] == 1) {
-                safeNodes.push_back(i);
-            }
+        vector<vector<int>> revGraph(graph.size());
+        vector<int> indegree(graph.size(),0);
+        for(int i = 0; i < graph.size(); i++){
+            if(graph[i].size()==0) safeNodes.push_back(i);
+            for (int j = 0; j < graph[i].size(); j++) {
+                revGraph[graph[i][j]].push_back(i);
+                indegree[i]++;
+            }          
         }
-        return safeNodes;
-    }
-
-    bool dfs(int node, vector<bool>& visited,vector<bool>& dfs_path, vector<vector<int>>& adj,vector<int>&  check) {
-        visited[node] = true;
-        dfs_path[node] = true;
-        for (int elem : adj[node]){
-            if (!visited[elem]){
-                if(dfs(elem,visited,dfs_path, adj,check)) {
-                    check[node] = 0;
-                    return true;
+        std::queue<int> q;
+        for(int i = 0; i < safeNodes.size(); i++ ){
+            q.push(safeNodes[i]);
+            while(!q.empty()){
+                int curr = q.front();
+                q.pop();
+                result.push_back(curr);
+                for(int elem : revGraph[curr]){
+                    indegree[elem]--;
+                    if(indegree[elem]==0) {
+                        q.push(elem);
+                    }
                 }
             }
-            else if(dfs_path[elem]){ 
-                return true;
-            }
-            }
-        check[node] = 1;
-        dfs_path[node] = false;
-        return false;
+        }
+        std::sort(result.begin(),result.end());
+        return result;        
     }
-        
 };
