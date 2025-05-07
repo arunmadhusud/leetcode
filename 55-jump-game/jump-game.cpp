bf4afbb38843eff0c453aaec0cc51808147ff858
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-05-07 12:12:59 -0400

    Time: 0 ms (100.00%) | Memory: 52.3 MB (64.71%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        std::unordered_set<int> u_set;
         int max_jump = nums[0];
         for(int i = 1; i < nums.size(); i++ ){
             if(max_jump>=nums.size()-1) return true;
