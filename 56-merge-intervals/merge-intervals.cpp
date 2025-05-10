cc3355bb6d264ac491334499c684b73775bdbb27
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-05-10 13:10:32 -0400

    Time: 5 ms (57.18%) | Memory: 23.9 MB (35.47%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -10,30 +10,15 @@ public:
         sort(intervals.begin(),intervals.end(),comp);
         int start = intervals[0][0];
         int end = intervals[0][1];
-        for (int i = 1; i < intervals.size(); i++){ 
-                       
+        for (int i = 1; i < intervals.size(); i++){                        
             if(intervals[i][0] > end){
-                vector<int> tmp(2,0);
-                tmp[0] = start;
-                tmp[1] = end;
-                result.push_back(tmp);
+                result.push_back({start,end});
                 start = intervals[i][0];
                 end = intervals[i][1];                
             }
-
-            else end = max(end,intervals[i][1]);
-
-            if(i == intervals.size()-1){
-                vector<int> tmp(2,0);
-                tmp[0] = start;
-                tmp[1] = end;
-                result.push_back(tmp);
-
-            }
-
-            
+            else end = max(end,intervals[i][1]);        
         }
-    
+        result.push_back({start,end});
         return result;        
     }
 };
