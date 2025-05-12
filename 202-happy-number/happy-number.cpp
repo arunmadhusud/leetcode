20ac3439273516a8d333fe2a59ef1ccca2728316
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-05-12 17:44:55 -0400

    Time: 0 ms (100.00%) | Memory: 8.5 MB (22.45%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,23 +1,46 @@
-class Solution {
-public:
-    bool isHappy(int n) {
-        int slwptr = n;
-        int fstptr = n;
-        while(fstptr!=1){
-            slwptr = sumNum(slwptr);
-            fstptr = sumNum(sumNum(fstptr));
-            if(fstptr==1) return true;
-            if(fstptr==slwptr) return false;
+// class Solution {
+// public:
+//     bool isHappy(int n) {
+//         int slwptr = n;
+//         int fstptr = n;
+//         while(fstptr!=1){
+//             slwptr = sumNum(slwptr);
+//             fstptr = sumNum(sumNum(fstptr));
+//             if(fstptr==1) return true;
+//             if(fstptr==slwptr) return false;
+//         }
+//         return true;        
+//     }
+//     int sumNum(int num){
+//         int sum = 0;
+//         while(num!=0){
+//             int digit = num % 10;
+//             sum += digit * digit;
+//             num = num /10;
+//         }
+//         return sum;
+//     }
+// };
+
+class Solution{
+    public:
+        int sumNum(int num){
+            int sum = 0;
+            while(num!=0){
+                int digit = num % 10;
+                sum += digit * digit;
+                num = num /10;
+            }
+            return sum;
         }
-        return true;        
-    }
-    int sumNum(int num){
-        int sum = 0;
-        while(num!=0){
-            int digit = num % 10;
-            sum += digit * digit;
-            num = num /10;
+        bool isHappy(int n){
+            std::unordered_set<int> u_set;
+            while(n!=1){
+                if(u_set.find(n)!=u_set.end()) return false;
+                u_set.insert(n);
+                n = sumNum(n);
+            }
+            return true;
+
         }
-        return sum;
-    }
 };
