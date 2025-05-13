93820bea7b86d6d76f952028154748d99d0070e0
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-05-12 23:31:22 -0400

    Time: 0 ms (100.00%) | Memory: 9.8 MB (81.35%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -7,15 +7,15 @@ public:
         return ans;
     }
 
-    void recur(int index, std::vector<int>& nums, std::vector<std::vector<int>>& ans, std::vector<int>& ds) {        
-        if(index==nums.size()){
-            ans.push_back(ds);
-            return;
+    void recur(int index, std::vector<int>& nums, std::vector<std::vector<int>>& ans, std::vector<int>& ds) {
+        // Add the current subset to the result
+        ans.push_back(ds);
+
+        // Generate all subsets starting from the current index
+        for (int i = index; i < nums.size(); i++) {
+            ds.push_back(nums[i]);         // Include nums[i] in the current subset
+            recur(i + 1, nums, ans, ds);  // Recurse to the next elements
+            ds.pop_back();                // Backtrack and remove nums[i]
         }
-        ds.push_back(nums[index]);
-        recur(index + 1, nums, ans, ds);
-        ds.pop_back(); 
-        recur(index + 1, nums, ans, ds);
-    
     }
 };
