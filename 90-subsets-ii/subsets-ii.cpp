ebbd0526712aa6019074ad72b486b84006efe42c
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-05-12 23:22:12 -0400

    Time: 3 ms (38.89%) | Memory: 10.3 MB (85.49%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        std::vector<std::vector<int>> ans;
+        std::vector<int> ds;
+        recur(0, nums, ans, ds);
+        return ans;
+    }
+
+    void recur(int index, std::vector<int>& nums, std::vector<std::vector<int>>& ans, std::vector<int>& ds) {       
+      
+        // if(index==nums.size()){
+        //     ans.push_back(ds);
+        //     return;
+        // }
+        ans.push_back(ds);
+        for(int i = index; i < nums.size(); i++){
+            if(i > index && nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            recur(i + 1, nums, ans, ds);
+            ds.pop_back();
+            // recur(i + 1, nums, ans, ds);
+        }
+   
+    }
+        
+};
