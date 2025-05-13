829f46eb3db5e5cd67f9dbd6988abda12dced977
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-05-13 00:13:39 -0400

    Time: 0 ms (100.00%) | Memory: 10.5 MB (69.85%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        std::vector<vector<int>> ans;
+        std::vector<std::vector<int>> ans;
         std::vector<int> ds;
-        std::vector<bool> freq(nums.size(), false);  // To track used elements
-        recur(0, nums, ds, ans, freq);
+        std::vector<bool> flag(nums.size(),false);
+        recur(nums,ans,ds,flag);
         return ans;
+        
     }
-
-    void recur(int index, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans, vector<bool>& freq) {
-        if (ds.size() == nums.size()) {
-            ans.push_back(ds);
-            // return;
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (!freq[i]) {  // Only proceed if the element has not been used
-                freq[i] = true;   // Mark as used
-                ds.push_back(nums[i]);  // Include the element
-
-                recur(index + 1, nums, ds, ans, freq);  // Recur for the next element
-
-                // Backtrack by undoing the previous choice
-                ds.pop_back();
-                freq[i] = false;  // Mark as unused
-            }
-        }
+    void recur(std::vector<int>& nums, std::vector<std::vector<int>>& ans, std::vector<int>& ds,std::vector<bool>& flag) {      
+  
+        if(ds.size()==nums.size()) ans.push_back(ds);
+        for(int i = 0; i < nums.size(); i++){
+            if(flag[i]) continue;
+            ds.push_back(nums[i]);
+            flag[i] = true;
+            recur(nums, ans, ds,flag);
+            ds.pop_back();
+            flag[i] = false;
+           }
+   
     }
 };
