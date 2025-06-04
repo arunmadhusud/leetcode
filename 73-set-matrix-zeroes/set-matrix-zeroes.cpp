c15b01d821e42a7a9c67c03a5b8cd38c7b659033
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-06-04 01:04:56 -0400

    Time: 0 ms (100.00%) | Memory: 18.6 MB (50.71%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,26 +3,55 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
+        bool firstRowZero = false, firstColZero = false;
 
-        vector<int> rowFlag(m,0);
-        vector<int> colFlag(n,0);
+        // Step 1: Check first row
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
 
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(matrix[i][j]==0){
-                    rowFlag[i]=1;
-                    colFlag[j]=1;
+        // Step 2: Check first column
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Step 3: Use first row and column as flags
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(rowFlag[i]==1 || colFlag[j]==1 ) matrix[i][j]=0;
+        // Step 4: Set cells based on markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
             }
         }
 
+        // Step 5: Handle first row
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
 
-        
+        // Step 6: Handle first column
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
