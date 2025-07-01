8e5be14c465de10b29c490b0a0e9b15301ee4633
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-07-01 19:13:05 -0400

    Time: 50 ms (5.07%) | Memory: 21.2 MB (44.52%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,21 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = {}
-        result = []
+        mp = defaultdict(list)
 
         for s in strs:
-            
             freq = [0] * 26
             for c in s:
                 freq[ord(c)-ord('a')] +=1
-
+                
             key = ""
             for f in freq:
                 key += str(f) +'#'
             
-            if key not in mp:
-                mp[key] = []
-
             mp[key].append(s)
 
            
