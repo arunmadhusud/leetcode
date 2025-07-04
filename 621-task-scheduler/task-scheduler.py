91899e3de7bbeb3437704332acaafcc8897fe791
Arun Madhusudhanan <118638562+arunmadhusud@users.noreply.github.com>
2025-07-04 17:50:08 -0400

    Time: 159 ms (21.72%) | Memory: 19.1 MB (64.70%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -3,18 +3,18 @@ class Solution:
         freq = Counter(tasks)
         pq = []
         for task,count in freq.items():
-            heapq.heappush(pq,[-count,task])
+            heapq.heappush(pq,-count)
         time = 0
         queue = []
         while pq or queue:
             time+=1
             if pq:
-                [count, elem] = pq[0]
+                count = pq[0]
                 heapq.heappop(pq)
                 if count+1 != 0:
-                    queue.append([time+n,count+1,elem])
+                    queue.append([time+n,count+1])
             while queue and queue[0][0]==time:
-                heapq.heappush(pq, [queue[0][1], queue[0][2]])
+                heapq.heappush(pq, queue[0][1])
                 queue.pop(0)
 
         
